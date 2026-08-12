cd4807d122b912ecc4c2bcb6fdd40392a5a85115
Sneh Faldu <145665013+SnehFaldu@users.noreply.github.com>
2026-08-12 21:28:05 +0530

    Create 240_Search_a_2D_Matrix_II.py

---
+++ Leetcode/240_Search_a_2D_Matrix_II.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:
+            return False
+
+        row = 0
+        col = len(matrix[0]) - 1
+
+        while row < len(matrix) and col >= 0:
+            value = matrix[row][col]
+
+            if value == target:
+                return True
+            elif value > target:
+                col -= 1
+            else:
+                row += 1
+
+        return False
