a7949f34ec3f47822e6894f579900e7917b1d954
Sneh Faldu <145665013+SnehFaldu@users.noreply.github.com>
2026-08-12 21:55:01 +0530

    Create 1985_Find_the_Kth_Largest_Integer_in_the_Array.py

---
+++ Leetcode/1985_Find_the_Kth_Largest_Integer_in_the_Array.py
@@ -0,0 +1,6 @@
+from typing import List
+
+class Solution:
+    def kthLargestNumber(self, nums: List[str], k: int) -> str:
+        nums.sort(key=lambda x: (len(x), x), reverse=True)
+        return nums[k - 1]
