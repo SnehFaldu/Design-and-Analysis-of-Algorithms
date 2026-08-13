29522f051610137e2209e7407cf55fccaab152fb
Sneh Faldu <145665013+SnehFaldu@users.noreply.github.com>
2026-08-13 22:29:33 +0530

    Create 372_Super_Pow.py

---
+++ Leetcode/372_Super_Pow.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def superPow(self, a: int, b: List[int]) -> int:
+        MOD = 1337
+
+        result = 1
+        for digit in b:
+            result = (pow(result, 10, MOD) * pow(a, digit, MOD)) % MOD
+
+        return result
