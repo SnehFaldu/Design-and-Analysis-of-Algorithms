5b6b489df828dc074d7f620cf521de057c6bf550
Sneh Faldu <145665013+SnehFaldu@users.noreply.github.com>
2026-08-16 13:20:26 +0530

    Create 973_K_Closest_Points_to_Origin.py

---
+++ Leetcode/973_K_Closest_Points_to_Origin.py
@@ -0,0 +1,6 @@
+from typing import List
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        points.sort(key=lambda p: p[0] * p[0] + p[1] * p[1])
+        return points[:k]
